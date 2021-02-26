bb57bac7fc2bc826e382cd62d24e07e0f318e7ca
QiDa <974830507@qq.com>
2021-02-26 22:32:17 +0800

    测试

---
+++ src/test/java/cn/sharepeople/test_ci/TestCiApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.sharepeople.test_ci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
